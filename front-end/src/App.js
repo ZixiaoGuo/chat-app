044c04976c97e9d3d68e0aabac4661d755c35fb0
Zixiao Guo <gzx964@gmail.com>
2022-07-28 17:11:23 -0700

    first commit

---
+++ front-end/src/App.js
@@ -0,0 +1,11 @@
+import { ChatEngine } from 'react-chat-engine';
+import './app.css';
+function App() {
+  return (
+    <ChatEngine height='100vh' projectID='' userName='' userSecret=''>
+
+    </ChatEngine>
+  );
+}
+
+export default App;
